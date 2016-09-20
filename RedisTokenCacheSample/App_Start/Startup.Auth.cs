56d2156664ac3a91791d64f629a34473e0b064eb
MariusR <sailingrock@live.com>
2016-09-19 18:57:47 -0700

    V1

---
+++ RedisTokenCacheSample/App_Start/Startup.Auth.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Cache;
+using System.IdentityModel.Claims;
+
+namespace RedisTokenCacheSample
+{
+    public partial class Startup
+    {
+        private static string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
+        private static string appKey = ConfigurationManager.AppSettings["ida:ClientSecret"];
+        public static string aadInstance = ConfigurationManager.AppSettings["ida:AADInstance"];
+        private static string tenantId = ConfigurationManager.AppSettings["ida:TenantId"];
+        private static string postLogoutRedirectUri = ConfigurationManager.AppSettings["ida:PostLogoutRedirectUri"];
+
+        public static readonly string Authority = aadInstance + tenantId;
+
+        // This is the resource ID of the AAD Graph API.  We'll need this to request a token to call the Graph API.
+        public static readonly string GraphResourceId = "https://graph.windows.net";
+        public static ClientCredential Credential;
+
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+            Credential = new ClientCredential(clientId, appKey);
+            app.UseOpenIdConnectAuthentication(
+                new OpenIdConnectAuthenticationOptions
+                {
+                    ClientId = clientId,
+                    Authority = Authority,
+                    PostLogoutRedirectUri = postLogoutRedirectUri,
+                    Notifications = new OpenIdConnectAuthenticationNotifications()
+                    {
+                        // If there is a code in the OpenID Connect response, redeem it for an access token and refresh token, and store those away.
+                        AuthorizationCodeReceived = async (context) =>
+                        {
+                            var code = context.Code;
+                            var credential = Credential;
+                            var signedInUserID = context.AuthenticationTicket.Identity.FindFirst(ClaimTypes.NameIdentifier).Value;
+                            var authContext = new AuthenticationContext(Authority, new RedisTokenCache(signedInUserID));
+                            var result = await authContext.AcquireTokenByAuthorizationCodeAsync(code, new Uri(HttpContext.Current.Request.Url.GetLeftPart(UriPartial.Path)), credential, GraphResourceId);
+                        }
+                    }
+                });
+        }
+    }
+}
---
+++ RedisTokenCacheSample/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Cache;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RedisTokenCacheSample.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public async Task<ActionResult> Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            var userId = ClaimsPrincipal.Current.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var tenantId = ClaimsPrincipal.Current.FindFirst("http://schemas.microsoft.com/identity/claims/tenantid").Value;
+            var userObjectID = ClaimsPrincipal.Current.FindFirst("http://schemas.microsoft.com/identity/claims/objectidentifier").Value;
+
+            var ctx = new AuthenticationContext(
+                Startup.aadInstance + tenantId, 
+                new RedisTokenCache(userId));
+            var authn = await ctx.AcquireTokenSilentAsync(
+                Startup.GraphResourceId, 
+                Startup.Credential, 
+                new UserIdentifier(userObjectID, UserIdentifierType.UniqueId));
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", authn.AccessToken);
+            var req = Startup.GraphResourceId + "/me?api-version=1.6";
+            var meJson = await client.GetStringAsync(req);
+            var me = new { mobile = "" };
+            me = JsonConvert.DeserializeAnonymousType(meJson, me);
+            ViewBag.Mobile = me.mobile;
+
+            return View();
+        }
+    }
+}
